e6b43e9d800ca5337885dec53e6b196c31d5f2f4
Pedro <elprogramadorpedro@gmail.com>
2021-11-04 15:33:24 -0300

    first deploy

---
+++ javascript_note_api/config/database.js
@@ -0,0 +1,22 @@
+/*const mongoose=require('mongoose')
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongodb://localhost/javascriptNote', {
+    userNewUrlParser:true,  //para utilizar a novas ferramentas de mongoose 
+    useUnifiedTopology:true, //indexsacao de conteudo 
+    useCreateIndex:true  //indexsacao de banco de ados 
+}).then(()=>console.log('Connection succesfull'))
+.catch((err)=>console.log(err));
+*/
+const mongoose= require ('mongoose')
+mongoose.Promise= global.Promise
+
+require('dotenv').config();
+const MONGO_URL= process.env.MONGO_URL
+
+mongoose.connect('MONGO_URL', {useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=>console.log('Conectado ao MongoDB'))
+    .catch((err)=>console.error(err));
+
+
+    
